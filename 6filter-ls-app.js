f19129d35b5bcb6b4a10d94284354935d37d6a13
Ashlin Aronin <ashilnaronin@gmail.com>
2015-10-20 17:52:06 -0700

    Refactor solution to filtered ls module

--- 6filter-ls-app.js
+++ 6filter-ls-app.js
@@ -1,20 +1,23 @@
 var filterls = require('./6filter-ls-module.js');
 
-// Can't directly assign return value because we are using asynchronous i/o
-// var filteredList = filterls(process.argv);
+// Get directory and filter params from CLI args
 var directory = process.argv[2];
 var filter = process.argv[3];
 
+// Call the module to actually do the filtering on the given directory with
+// the given filter.
 filterls(directory, filter, callback);
 
-// Is this proper setup for callback?
-function callback(err, data) {
+// Define what happens when data or and error is received from the module.
+// In this case, just log either to the console.
+function callback(err, list) {
+  // Follow same early return on error pattern as in module
   if (err) {
-    console.log(err)
-  } else {
-
-    data.forEach(function(item) {
-      console.log(item);
-    });
+    return console.log("There was an error: ", err);
   }
+
+  // No error, continue to print out the result
+  list.forEach(function(filename) {
+    console.log(filename);
+  });
 }
--- 6filter-ls-module.js
+++ 6filter-ls-module.js
@@ -1,26 +1,29 @@
 var fs = require('fs');
 var path = require('path');
 
-
 module.exports = function (directory, filter, callback) {
-  // try to read the dir and send to teh callback
+  // Try to read the dir and send to the callback
   fs.readdir(directory, readdir_callback);
 
-
-  // callback gets two params, first will be error (could be null) and data
+  // This callback gets two params. The first will be error (could be null).
+  // The second in this case is the list of files in the directory from fs.readdir
   function readdir_callback(err, list) {
+    // Early error return pattern. If we get an error we send it back up through
+    // the chain of callbacks. If there's no error, continue processing data.
     if (err) {
-      return callback(err); // early return on error
+      return callback(err);
     }
 
+    // Filter the file results by the given filter and add them to a new array
     var filteredList = [];
-
     list.forEach(function (file) {
-      if (path.extname(file) == '.' + filter) {
+      if (path.extname(file) === '.' + filter) {
         filteredList.push(file);
       }
     });
 
+    // Processing went smoothly, call whichever callback was passed to this module
+    // and send the filtered list to it
     callback(null, filteredList);
   }
 }
