5097925d59b8ecdd0df529863416d42f7459ecff
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-01 15:24:01 -0800

    Finish exercise 7: simple http client

--- 7http.js
+++ 7http.js
@@ -1,7 +1,31 @@
+/*
+** Write a program that performs an HTTP GET request to a URL provided to you
+** as the first command-line argument. Write the String contents of each "data"
+** event from the response to a new line on the console (stdout).
+*/
 var http = require('http');
 
-http.get(url, callback);
+http.get(process.argv[2], callback);
 
-function callback (response) {
-  
+function callback(response) {
+  // Response object is a Node Stream object
+  // Set its encoding to utf8 so it outputs strings rather than node Buffers
+  response.setEncoding("utf8");
+
+  response.on("data", function(data) {
+    console.log(data);
+  });
+
+  response.on("error", function(error) {
+    console.log(error);
+  });
+
+  // Apparently you can also do it like this.
+  // Cool that you can pass the data directly to console.log.
+  /* http.get(process.argv[2], function (response) {
+       response.setEncoding('utf8')
+       response.on('data', console.log)
+       response.on('error', console.error)
+     })
+  */
 }
